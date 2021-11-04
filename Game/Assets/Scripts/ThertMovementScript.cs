cdcc6b0cfd7c4d4e0ab96f52cb264032ca9eba07
perigelios <vit14233@gmail.com>
2021-11-04 18:46:46 +0800

    Add project

---
+++ Game/Assets/Scripts/ThertMovementScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThertMovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float jumpPower = 500f;
+    public float speed = 10f;
+    float turnSmoothVelocity;
+    public bool ground;
+    public Rigidbody rb;
+
+
+    void Update()
+    {
+        rb = GetComponent<Rigidbody>();
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothVelocity);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (ground == true)
+            {
+                rb.AddForce(transform.up * jumpPower);
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            ground = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            ground = false;
+        }
+    }
+}
