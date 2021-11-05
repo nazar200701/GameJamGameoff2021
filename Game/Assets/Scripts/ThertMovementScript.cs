81441e7b9a5f8738e490d882e5e6f20ea1d8d3d3
perigelios <vit14233@gmail.com>
2021-11-05 20:19:31 +0800

    3

--- Game/Assets/Scripts/ThertMovementScript.cs
+++ Game/Assets/Scripts/ThertMovementScript.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody))]
 public class ThertMovementScript : MonoBehaviour
 {
-    public CharacterController controller;
+    
     public Transform cam;
 
     public float jumpPower = 500f;
@@ -13,44 +15,81 @@ public class ThertMovementScript : MonoBehaviour
     public bool ground;
     public Rigidbody rb;
 
-
-    void Update()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+    }
+    void FixedUpdate()
+    {
+        MovementLogic();
+        JumpLogic();
+    }
+    private void MovementLogic()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
-        if(direction.magnitude >= 0.1f)
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+
+        transform.Translate(movement * speed * Time.fixedDeltaTime);
+
+        //Vector3 direction = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
+
+        if (movement.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothVelocity);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            //controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    //void Update()
+    //{
+    //    float horizontal = Input.GetAxisRaw("Horizontal");
+    //    float vertical = Input.GetAxisRaw("Vertical");
+    //    Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+    //    if (direction.magnitude >= 0.1f)
+    //    {
+    //        float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+    //        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothVelocity);
+    //        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+    //        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+    //        controller.Move(moveDir.normalized * speed * Time.deltaTime);
+    //    }
+
+
+    //}
+    private void JumpLogic()
+    {
+        if (Input.GetAxis("Jump") > 0)
         {
-            if (ground == true)
+            if (ground)
             {
-                rb.AddForce(transform.up * jumpPower);
+                rb.AddForce(Vector3.up * jumpPower);
             }
         }
     }
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            ground = true;
-        }
+        IsGroundedUpate(collision, true);
     }
-    private void OnCollisionExit(Collision collision)
+
+    void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground")
+        IsGroundedUpate(collision, false);
+    }
+
+    private void IsGroundedUpate(Collision collision, bool value)
+    {
+        if (collision.gameObject.tag == ("Ground"))
         {
-            ground = false;
+            ground = value;
         }
     }
 }
